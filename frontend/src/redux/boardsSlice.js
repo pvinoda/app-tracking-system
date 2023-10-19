c7f0b28f60f73be85d39ee30f28227698ee320f1
Sharansrj567 <sharansrj567@gmail.com>
2023-10-19 18:54:32 -0400

    add,edit,delete,update board actions in boardslice redux store

--- frontend/src/redux/boardsSlice.js
+++ frontend/src/redux/boardsSlice.js
@@ -30,6 +30,38 @@ const boardsSlice = createSlice({
     deleteInitialData: (state, action) => {
       state = [];
     },
+    addBoard: (state, action) => {
+      const isActive = state.length > 0 ? false : true;
+      const payload = action.payload;
+      const board = {
+        name: payload.name,
+        isActive,
+        columns: [],
+      };
+      board.columns = payload.newColumns;
+      state.push(board);
+      createNewBoard(state);
+    },
+    editBoard: (state, action) => {
+      const payload = action.payload;
+      const board = state.find((board) => board.isActive);
+      board.name = payload.name;
+      board.columns = payload.newColumns;
+      createNewBoard(state);
+    },
+    deleteBoard: (state) => {
+      const board = state.find((board) => board.isActive);
+      state.splice(state.indexOf(board), 1);
+      createNewBoard(state);
+    },
+    setBoardActive: (state, action) => {
+      state.map((board, index) => {
+        index === action.payload.index
+          ? (board.isActive = true)
+          : (board.isActive = false);
+        return board;
+      });
+    },
   },
 });
 
