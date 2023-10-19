884d17fb275eafd10775c25702f4b20f3526687a
Sharansrj567 <sharansrj567@gmail.com>
2023-10-19 17:09:38 -0400

    add getBoardsData function and call it whenever app starts using hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,58 @@
 import React, { useState, useEffect } from "react";
 import ApplicationPage from "./application";
 import LoginPage from "./login/LoginPage";
+import $ from "jquery";
 
 function App() {
   const [auth, setAuth] = useState(false);
+
+  const getDataFunction = () => {
+    $.ajax({
+      url: "http://localhost:5000/getBoards",
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Credentials": "true",
+      },
+      credentials: "include",
+      data: JSON.stringify({
+        getData: "getData",
+      }),
+      success: (boards) => {
+        console.log("BOARDS", boards);
+      },
+    });
+  };
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     setCurrentPage(
       <LoginPage
-        side={() => setCurrentPage(<ApplicationPage logout={handleLogout} />)}
+        side={() =>
+          setCurrentPage(
+            <ApplicationPage getData={getDataFunction} logout={handleLogout} />
+          )
+        }
       />
     );
     setAuth(false);
   };
   const [currentPage, setCurrentPage] = useState(
     <LoginPage
-      side={() => setCurrentPage(<ApplicationPage logout={handleLogout} />)}
+      side={() =>
+        setCurrentPage(
+          <ApplicationPage getData={getDataFunction} logout={handleLogout} />
+        )
+      }
     />
   );
   useEffect(() => {
     if (localStorage.getItem("token")) {
       setAuth(true);
-      setCurrentPage(<ApplicationPage logout={handleLogout} />);
+      setCurrentPage(
+        <ApplicationPage getData={getDataFunction} logout={handleLogout} />
+      );
     }
   }, [auth]);
 
