2cd4c68a23a24154bcda8f3ac5cccc4e444cc9ba
Sharansrj567 <sharansrj567@gmail.com>
2023-10-19 21:39:33 -0400

    fix test boards api

--- backend/test_app.py
+++ backend/test_app.py
@@ -32,7 +32,7 @@ def client():
         password = info["password"]
         app.config["MONGODB_SETTINGS"] = {
             "db": "appTracker",
-            "host": f'mongodb+srv://{username}:{password}@cluster0.en3fo.mongodb.net/todolistDB?retryWrites=true&w=majority',
+            "host": f'mongodb+srv://{username}:{password}@cluster0.en3fo.mongodb.net/todolistDB?retryWrites="true"&w=majority',
         }
     db = MongoEngine()
     db.disconnect()
@@ -51,7 +51,8 @@ def user(client):
     :return: the user object and auth token
     """
     # print(request.data)
-    data = {"username": "newest123", "password": "123456", "fullName": "fullName"}
+    data = {"username": "newest123",
+            "password": "123456", "fullName": "fullName"}
 
     user = Users.objects(username=data["username"])
     user.first()["applications"] = []
@@ -76,7 +77,7 @@ def test_alive(client):
 
 
 # 3. testing if the application is getting data from database properly
-def test_boards_api(client, user):
+def test_getBoards_api(client, user):
     """
     Tests that using the application GET endpoint returns data
 
@@ -93,7 +94,7 @@ def test_boards_api(client, user):
 
 
 # 4. testing if the application is saving data in database properly
-def test_get_boards(client, mocker, user):
+def test_get_boards(client, user):
     """
     Tests that using the application POST endpoint saves data
 
@@ -101,30 +102,15 @@ def test_get_boards(client, mocker, user):
     :param user: the test user object
     """
 
-    mocker.patch(
-        # Dataset is in slow.py, but imported to main.py
-        "app.get_new_user_id",
-        return_value=-1,
-    )
     user, header = user
-    user["applications"] = []
+    user["boards"] = []
     user.save()
-    # mocker.patch(
-    #     # Dataset is in slow.py, but imported to main.py
-    #     'app.Users.save'
-    # )
     rv = client.post(
-        "/applications",
+        "/boards",
         headers=header,
-        json={
-            "application": {
-                "jobTitle": "fakeJob12345",
-                "companyName": "fakeCompany",
-                "date": str(datetime.date(2021, 9, 23)),
-                "status": "1",
-            }
-        },
+        json={"board":
+              [{"columns": [{"id": "915ab7d3-cbcd-4953-99b2-ed9b20008804", "name": "Applied", "tasks": [{"description": "", "status": "Rejected", "subtasks": [{"isCompleted": "true", "title": "Online Assessment"}, {"isCompleted": "true", "title": "Technical Interview"}, {"isCompleted": "false", "title": "HR Interview"}], "title": "Google"}, {"title": "New Company", "description": "123456", "subtasks": [{"title": "1", "isCompleted": "false", "id": "be5f01ab-6831-419c-a422-1250ff920107"}, {"title": "2", "isCompleted": "false", "id": "42cffd8e-98f0-44b7-bc66-43509341ac29"}], "status": "Applied"}]}, {"id": "d3214ced-1984-4284-b672-fc57cde0e826", "name": "Rejected", "tasks": [{"description": "", "status": "Rejected", "subtasks": [{"isCompleted": "true", "title": "Online Assessment"}, {"isCompleted": "false", "title": "Behavioural Round"}, {"isCompleted": "false", "title": "HR Interview"}], "title": "JP Morgan Chase"}]}, {"id": "6c312412-066b-4423-83d6-e92bcf4e0084", "name": "Waiting For Referral", "tasks": [{"description": "", "status": "Waiting For Senior Reply", "subtasks": [], "title":"Amazon"}]}, {"id": "a1f7e8a3-ab43-424c-8b7f-4f5bd521d423", "name": "On Hold", "tasks": [{"description": "", "status": "On Hold", "subtasks": [{"id": "a10f9215-7a3f-4c79-957c-35e2fb621236", "isCompleted": "false", "title": "Phone Interview"}, {"id": "c4b4c314-e86e-4cc5-b7aa-02ca749a0a6c", "isCompleted": "false", "title": "Resume round"}], "title": "Lexisnexis"}]}], "isActive": "true", "name": "SDE Applications"}
+               ]
+              }
     )
     assert rv.status_code == 200
-    jdata = json.loads(rv.data.decode("utf-8"))["jobTitle"]
-    assert jdata == "fakeJob12345"
